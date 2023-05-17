d10d53406530122adf560bb25aabfec909de4302
robinplancon <robin.plancon@gmail.com>
2023-05-17 16:28:35 +0200

    challenge fini.

--- js/exos.js
+++ js/exos.js
@@ -6,12 +6,31 @@
 */
 
 /* Exo 1 */
-// Ton code ici...
-
+const question1 = "Quelle mer borde la ville de Sébastopol ?";
+let solution1 = "la mer Noire";
+let good_answers_counter = 0;
 
 /* Exo 2 */
 // Ton code ici...
-
+reponse1 = prompt(question1);
+if (reponse1 === solution1 ){
+    alert("Gagné !");
+    good_answers_counter += 1;
+} else {
+    alert("Perdu ...");
+}
 
 /* Exo 3 */
 // Ton code ici...
+const question2 = "Quel est l'âge du capitaine ?";
+let solution2 = 63;
+
+reponse2 = prompt(question2);
+if (parseInt(reponse2) === solution2 ){
+    alert("Gagné !");
+    good_answers_counter += 1;
+} else {
+    alert("Perdu ...");
+}
+
+alert(`Vous avez donné ${good_answers_counter} bonnes réponses.`);
